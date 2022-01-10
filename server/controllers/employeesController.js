eca7e89913ad7bbf403da5a4be6c125f20fced61
TangAdorable <sunisa.techa@outlook.co.th>
2022-01-11 00:32:00 +0700

    update
    server employees
    create api

--- server/controllers/employeesController.js
+++ server/controllers/employeesController.js
@@ -1,6 +1,6 @@
 const employees = require("../models/employees")
 
-
+    //บันทึกข้อมูล
 exports.create=(req,res)=>{
     const {UserID,FirstName,LastName,JobTitle,Department,CreatorBy}=req.body
 
@@ -19,12 +19,60 @@ exports.create=(req,res)=>{
         case !CreatorBy:
             return res.status(400).json({error:"ระบุชื่อผู้บันทึกข้อมูล"})
     }
-    //บันทึกข้อมูล
+
     employees.create({UserID,FirstName,LastName,JobTitle,Department,CreatorBy},(err,create)=>{
         if(err){
             res.status(400).json({error:"พบข้อมูล UserID นี้ในระบบ"})
         }
         res.json(create)
     })
-
 }
+
+    //แสดงข้อมูล
+exports.getallemp=(req,res)=>{
+    employees.find({}).exec((err,allemp)=>{
+        res.json(allemp)
+    })
+}
+
+
+exports.singleemp=(req,res)=>{
+    const {UserID} = req.params
+    employees.findOne({UserID}).exec((err,emp)=>{
+        res.json(emp)
+    })
+}
+
+exports.removeemp=(req,res)=>{
+    const {UserID} = req.params
+    employees.findOneAndRemove({UserID}).exec((err,emp)=>{
+        if(err) console.log(err)
+        res.json({
+            message:"success delete user"
+        })
+    })
+}
+
+exports.updateemp=(req,res)=>{
+    const {UserID} = req.params
+    const {FirstName,LastName,JobTitle,Department}=req.body //ส่งข้อมูลมา
+    
+    switch(true){
+        case !FirstName:
+            return res.status(400).json({error:"ระบุชื่อพนักงาน"})
+        case !LastName:
+            return res.status(400).json({error:"ระบุนามสกุลพนักงาน"})
+        case !JobTitle:
+            return res.status(400).json({error:"ระบุตำแหน่งของพนักงาน"})
+        case !Department:
+            return res.status(400).json({error:"ระบุแผนกของพนักงาน"})
+    }
+       
+    employees.findOneAndUpdate({UserID},{FirstName,LastName,JobTitle,Department},{new:true})
+    .exec((err,emp)=>{
+        if(err) console.log(err)
+        res.json(emp)
+        
+    })
+}
+
--- server/route/employees.js
+++ server/route/employees.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {create} = require("../controllers/employeesController")
+const employees = require("../controllers/employeesController")
 
-router.post('/create',create)
+router
+.post('/create',employees.create)
+.get('/allemp',employees.getallemp)
+.get('/emp/:UserID',employees.singleemp)
+.delete('/emp/:UserID',employees.removeemp)
+.put('/emp/:UserID',employees.updateemp)
 
 
 module.exports=router
+

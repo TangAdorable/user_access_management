6c3889024c124e4eb87c1bc39ab61256c0cebb19
TangAdorable <sunisa.techa@outlook.co.th>
2022-02-16 00:13:30 +0700

    update
    client : ทำหน้า AddAppLog

--- client/src/pages/management/AddAppLog.js
+++ client/src/pages/management/AddAppLog.js
@@ -6,7 +6,15 @@ import Swal from "sweetalert2";
 
 export default (props) => {
 
-    const [empUser, setempUser] = useState('')
+    const [empUser, setempUser] = useState({
+        UserID: '',
+        App_name: '',
+        Access: '',
+        App_status: false,
+        TicketID: '',
+        Note: '',
+    })
+    const { UserID, App_name, Access, App_status, TicketID, Note } = empUser;
 
     const empSingleUser = () => {
         axios
@@ -17,82 +25,94 @@ export default (props) => {
             .catch(err => console.log(err))
     }
 
+    // const List
+
 
 
     useEffect(() => {
         empSingleUser()
     }, [])
 
-    // const FormModifyLog = () => (
-    //     <Form className="col-7" onSubmit={submitForm}>
-    //       <Form.Group className="mb-3">
-    //         <Form.Label>User ID :</Form.Label>
-    //         <Form.Control type="text" readOnly rows="3" value={UserID} />
-    //       </Form.Group>
-    
-    //       <Form.Group className="mb-3">
-    //         <Form.Label>Application Name :</Form.Label>
-    //         <Form.Control type="text" readOnly rows="3" value={App_name} />
-    //       </Form.Group>
-    
-    //       <Form.Group className="mb-3">
-    //         <Form.Label>Access :</Form.Label>
-    //         <Form.Control type="text" readOnly rows="3" value={Access} />
-    //       </Form.Group>
-    
-    //       <Form.Group className="mb-3">
-    //         <Form.Label>Application Access Status :</Form.Label>
-    //         <Form.Check
-    //           className="ms-3"
-    //           checked={App_status}
-    //           onChange={checkedValue('App_status')}
-    //           value={!App_status}
-    //           label="Active"
-    //         />
-    //         <div className="ms-3 fs-6">Status is {App_status ? 'Enable' : 'Disable'}.</div>
-    
-    //       </Form.Group>
-    
-    //       <Form.Group className="mb-3">
-    //         <Form.Label>Ticket ID :</Form.Label>
-    //         <Form.Control type="text" rows="3" value={TicketID} onChange={inputValue('TicketID')} />
-    //       </Form.Group>
-    
-    //       <Form.Group className="mb-3">
-    //         <Form.Label>Note :</Form.Label>
-    //         <Form.Control as="textarea" rows="3" value={Note} onChange={inputValue('Note')} />
-    //       </Form.Group>
-    
-    //       <React.Fragment>
-    //         <Button type="submit" variant="warning" className="m-1">
-    //           Update
-    //         </Button>
-    //       </React.Fragment>
-    //     </Form>
-    //   );
 
-    //   const submitForm = (e) => {
-    //     e.preventDefault(); //e.preventDefault() ถูกใช้เพื่อไม่ให้ browser reload หรือ refresh
-    //     // console.table({title,content,author})
-    //     //console.log("API URL = ", process.env.REACT_APP_API)
-    //     axios
-    //       .put(`${process.env.REACT_APP_API_Userlogs}/updatelog/${props.match.params._id}`, {
-    //         App_status,
-    //         TicketID,
-    //         Note,
-    //       })
-    //       .then((res) => {
-    //         console.log(res)
-    //         Swal.fire('แจ้งเตือน', 'Modify data success', 'success')
-    //         //return <Redirect to={Routes.NotFound.path} />
-    //         // const {App_status, TicketID, Note} = response.data
-    //         // setSingleLog({...singleLog,App_status, TicketID, Note})
-    
-    //       })
-    //       .catch((err) => {
-    //         alert(err);
-    //       });
-    //   };
+    //กำหนดค่าให้กับ state
+    const inputValue=name=>event=>{
+        // console.log(name,"=",event.target.value)
+        //เก็บข้อมูล state เป็นแบบ object
+        setempUser({...empUser,[name]:event.target.value});
+      }
+
+    const checkedValue = (name) => (event) => {
+        setempUser({ ...empUser, [name]: event.target.checked });
+    };
+
+
+    const FormModifyLog = () => (
+        <Form className="col-7" onSubmit={submitForm}>
+            <Form.Group className="mb-3">
+                <Form.Label>User ID :</Form.Label>
+                <Form.Control type="text" readOnly rows="3" value={UserID} />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Application Name :</Form.Label>
+                <Form.Control type="text" rows="3" value={App_name} onChange={inputValue('App_name')} />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Access :</Form.Label>
+                <Form.Control type="text" rows="3" value={Access} onChange={inputValue('Access')} />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Application Access Status :</Form.Label>
+                <Form.Check
+                    className="ms-3"
+                    checked={App_status}
+                    onChange={checkedValue('App_status')}
+                    value={!App_status}
+                />
+                <div className="ms-3 fs-6">Status is {App_status ? 'Enable' : 'Disable'}.</div>
+
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Ticket ID :</Form.Label>
+                <Form.Control type="text" rows="3" value={TicketID}  onChange={inputValue('TicketID')} />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Note :</Form.Label>
+                <Form.Control as="textarea" rows="3" value={Note}  onChange={inputValue('Note')} />
+            </Form.Group>
+
+            <React.Fragment>
+                <Button type="submit" variant="warning" className="m-1">
+                    Submit
+                </Button>
+            </React.Fragment>
+        </Form>
+    );
+
+    const submitForm = (e) => {
+        e.preventDefault(); //e.preventDefault() ถูกใช้เพื่อไม่ให้ browser reload หรือ refresh
+        // console.table({title,content,author})
+        //console.log("API URL = ", process.env.REACT_APP_API)
+        axios
+            .post(`${process.env.REACT_APP_API_Userlogs}/addlogs`, {
+                UserID,App_name,Access,App_status,TicketID,Note
+            })
+            .then((res) => {
+                console.log(res)
+                Swal.fire('แจ้งเตือน', 'บันทึกข้อมูลเรียบร้อยแล้ว', 'success')
+                //return <Redirect to={Routes.NotFound.path} />
+                // const {App_status, TicketID, Note} = response.data
+                // setSingleLog({...singleLog,App_status, TicketID, Note})
+
+            })
+            .catch((err) => {
+                Swal.fire('แจ้งเตือน', err.response.data.error, 'error')
+            });
+    };
 
 
 
@@ -101,7 +121,7 @@ export default (props) => {
             <UserProfile {...empUser} />
             {/* {JSON.stringify(props)} */}
             <h5 className='mb-3'>Add user application log</h5>
-            {/* {FormModifyLog()} */}
+            {FormModifyLog()}
         </div>
     );
 };

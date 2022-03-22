4bc272df08ef40473d8048851813f396a3d698f0
TangAdorable <sunisa.techa@outlook.co.th>
2022-03-22 10:57:50 +0700

    update
    เพิ่ม requireLogin ใน api ของ app_system
    clear Sidebar menu

--- server/route/app_system.js
+++ server/route/app_system.js
@@ -4,9 +4,9 @@ const app_system = require("../controllers/appSystemController")
 const {requireLogin} = require("../controllers/authController")
 
 router
-.post('/create',app_system.create)
-.get('/allapp',app_system.allapp)
-.put('/updateSingleApp/:app_name',app_system.updateSingleApp)
+.post('/create',requireLogin,app_system.create)
+.get('/allapp',requireLogin,app_system.allapp)
+.put('/updateSingleApp/:app_name',requireLogin,app_system.updateSingleApp)
 
 module.exports=router
 
